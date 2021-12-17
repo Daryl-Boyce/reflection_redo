f1e0588ae9c361e68ce3452d6fae29a17a5956eb
Dary-Boyce <contact@darylboyce.com>
2021-12-17 11:04:33 +0000

    Basic sticky header added

---
+++ main.js
@@ -0,0 +1,12 @@
+// Makes the header stick and return on scroll up
+window.addEventListener('wheel', function(event)
+{
+ if (event.deltaY < 0)
+ {
+    document.querySelector('.header__sticky').classList.add('sticky');
+ }
+ else if (event.deltaY > 0)
+ {
+    document.querySelector('.header__sticky').classList.remove('sticky');
+ }
+});
