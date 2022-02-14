ae53a74cb601d0b182ec2900376e4f75a1436b8d
Dary-Boyce <contact@darylboyce.com>
2022-02-14 13:32:56 +0000

    Added callable header and footer.

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="stylesheet" href="css/styles.css">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;1,100;1,200&display=swap"
-		rel="stylesheet">
-	<script src="https://kit.fontawesome.com/8770348bdd.js" crossorigin="anonymous">
-	</script>
-	<title>Netmatters | Full Service Digital Agency | Norwich, Norfolk | Netmatters</title>
-</head>
+<!-- This calls the header and footer files and uses them on the page. Can be used across all pages wouth the need to add the footer and header -->
+<?php
+	include('header.php');
+	include('footer.php');
+?>
 
 <body>
 
@@ -790,140 +780,7 @@
 			<!--  -->
 		</div>
 	<!--  -->
-	<!-- Footer -->
-	<footer >
-		<div class="footer__inner">
-			<!-- Inner sevices and about section -->
-			<div class="footer__inner__about__services">
-				<div class="footer__about">
-					<h3>ABOUT NETMATTERS</h3>
-					<ul class="footer__about__list">
-						<li class="footer_about__list_item">News
-						</li>
-						<li class="footer_about__list_item">Our Careers
-						</li>
-						<li class="footer_about__list_item">Our Team
-						</li>
-						<li class="footer_about__list_item">Our Office Tour
-						</li>
-						<li class="footer_about__list_item">Privacy Policy
-						</li>
-						<li class="footer_about__list_item">Cookie Policy
-						</li>
-						<li class="footer_about__list_item">Terms &copy; Conditions
-						</li>
-						<li class="footer_about__list_item">Enviromental Policy
-						</li>
-					</ul>
-				</div>
-				<!--  -->
-				<div class="footer__our__services">
-					<h3>OUR SERVICES</h3>
-					<ul class="footer__our__services__list">
-						<li class="footer_our__services__list_item">Bespoke Software
-						</li>
-						<li class="footer_our__services__list_item">IT Support
-						</li>
-						<li class="footer_our__services__list_item">Digital Marketing
-						</li>
-						<li class="footer_our__services__list_item">Telecoms Services
-						</li>
-						<li class="footer_our__services__list_item">Web Design
-						</li>
-						<li class="footer_our__services__list_item">Cyber Security
-						</li>
-						<li class="footer_our__services__list_item">Developer Training
-						</li>
-					</ul>
-				</div>
-			</div>
-			<!--  -->
-			<!--  -->
-			<div class="footer__inner__branch">
-				<div class="footer__cambridge__branch">
-					<h3>CAMBRIDGE OFFICE</h3>
-					<ul class="footer__cambridge__branch__list">
-						<li class="footer__cambridge__branch__list__item">Unit 1.28,
-						</li>
-						<li class="footer__cambridge__branch__list__item">St John's Innovation Centre,
-						</li>
-						<li class="footer__cambridge__branch__list__item">Cowley Road, Milton,
-						</li>
-						<li class="footer__cambridge__branch__list__item">Cambridge,
-						</li>
-						<li class="footer__cambridge__branch__list__item">CB4 0WS
-						</li>
-					</ul>
-					<div>Tel: 01223 37 57 72</div>
-				</div>
-				<!--  -->
-				<div class="footer__wymondham__branch">
-					<h3>WYMONDHAM OFFICE</h3>
-					<ul class="footer__wymondham__branch__list">
-						<li class="footer__wymondham__branch__list__item">Unit 15,
-						</li>
-						<li class="footer__wymondham__branch__list__item">Penfold Drive,
-						</li>
-						<li class="footer__wymondham__branch__list__item">Gateway 11 Business Park,
-						</li>
-						<li class="footer__wymondham__branch__list__item">Wymondham, Norfolk,
-						</li>
-						<li class="footer__wymondham__branch__list__item">NR18 0WZ
-						</li>
-					</ul>
-					<div>Tel: 01603 70 40 20</div>
-				</div>
-				<!--  -->
-				<div class="footer__great__yarmouth__branch">
-					<h3>GREAT YARMOUTH OFFICE</h3>
-					<ul class="footer__great__yarmouth__branch__list">
-						<li class="footer__great__yarmouth__branch__list__item">Suite F23,
-						</li>
-						<li class="footer__great__yarmouth__branch__list__item">Beacon Innovation Centre,
-						</li>
-						<li class="footer__great__yarmouth__branch__list__item">Beacon Park, Gorleston,
-						</li>
-						<li class="footer__great__yarmouth__branch__list__item">Great Yarmouth, Norfolk,
-						</li>
-						<li class="footer__great__yarmouth__branch__list__item">NR31 7RA
-						</li>
-					</ul>
-					<div>Tel: 01493 60 32 04</div>
-				</div>
-			</div>
-			<!--  -->
-			<hr>
-			<div class="footer__inner__lower">
-				
-					<p>&copy; Copyright Netmatters Ltd. 2021 -
-						All rights reserved</p>
-					
-					<a href="">Sitemap</a>
-					
-					<div class="social__media">
-						<div class="social__media__buttons">
-							<div class="social__media__buttons__twitter">
-								<a href="#"><i class="fab fa-facebook-f"></i></a>
-							</div>
-							<div class="social__media__buttons__facebook">
-								<a href="#"><i class="fab fa-twitter"></i></a>
-							</div>
-							<div class="social__media__buttons__instagram">
-								<a href="#"><i class="fab fa-instagram"></i></a>
-							</div>
-							<div class="social__media__buttons__linkedin">
-								<a href="#"><i class="fab fa-linkedin-in"></i></a>
-							</div>
-						</div>
-					</div>
-					
-				
-			</div>
-
-			<!--  -->
-			
-		</div>
-	</footer>
+	
 			<!--  -->
 			<!-- <div class="awards__and__partners">
 				<ul>
