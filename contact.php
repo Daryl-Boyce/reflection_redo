5d0ebff44990ffa31d2100798c199cfb6e894240
Dary-Boyce <contact@darylboyce.com>
2022-02-18 13:30:51 +0000

    Added base for contact page

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!-- This calls the header files and uses them on the page. Can be used across all pages without the need to add the header -->
+<?php
+$cards = [];
+
+
+	include('header.php');
+	include ('db__connection.php');
+
+
+
+$conn = OpenCon();
+
+
+?>
+<!--  -->
+<!--  -->
+<!--  -->
+<body>
+    
+</body>
+<!--  -->
+<!--  -->
+<!--  -->
+<!-- This calls the footer files and uses them on the page. Can be used across all pages without the need to add the footer -->
+<?php
+	include('footer.php');
+?>
